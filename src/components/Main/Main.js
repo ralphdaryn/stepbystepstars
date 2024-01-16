9581ac4c1dbd40129419a94c9c99a0096531a4ec
Ralph <ralphdarync@gmail.com>
2024-01-16 00:17:55 -0500

    update main text

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,13 +4,27 @@ const Main = () => {
   return (
     <div className="main">
       <div className="main__container">
-        <h1 className="main__text">Elevate Your Celebration and Wellness!</h1>
-        <p className="main__subtext">
-          Step By Step Stars offers dynamic dance parties, artistic face
-          painting for birthdays, along with engaging musical experiences for
-          the little ones and fitness classes for moms.
-        </p>
-        <p className="main__subtext">Join us for unforgettable experiences!</p>
+        <div>
+          <h2 className="main__text">
+            Elevate Your Celebration and Wellness with
+          </h2>
+          <h1 className="main__text">Step By Step Stars!</h1>
+        </div>
+        <p className="main__subtext">We offer the following services:</p>
+        <div>
+          <ul>
+            <li>DANCE PARTIES (Ages 4+)</li>
+            <li>FACE PAINTING </li>
+            <li>MUSICAL BABIES (Ages 3 and under)</li>
+            <li>FITNESS CLASSES for MOMS</li>
+          </ul>
+        </div>
+        <div>
+          <p className="main__subtext">
+            Join us for unforgettable experiences!
+          </p>
+        </div>
+        <button>Learn More</button>
         <button>Book Now!</button>
       </div>
     </div>
