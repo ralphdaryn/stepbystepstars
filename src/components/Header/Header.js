537bcad4930d7b53a41e7536df8d7b289e079dc2
Ralph <ralphdarync@gmail.com>
2024-01-16 19:38:19 -0500

    update navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,16 @@ const Header = () => {
     <div className="header">
       <div className="header__container">
         <img className="header__logo" src={logo} alt="step by step logo" />
-        <FaBars size={30} />
+        <FaBars className="header__navbar" size={30} />
+        <nav className="header__navigation">
+          <ul className="header__navigation-list">
+            <li className="header__navigation-link">Home</li>
+            <li className="header__navigation-link">About</li>
+            <li className="header__navigation-link">Services</li>
+            <li className="header__navigation-link">Gallery</li>
+            <li className="header__navigation-link">Contact</li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
