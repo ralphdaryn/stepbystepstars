95ca39df0bb9144d458ff09c422ad689752fe711
Ralph <ralphdarync@gmail.com>
2024-01-16 21:03:14 -0500

    update navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,23 @@
 import "./Header.scss";
+import { useState } from "react";
 import logo from "../../assets/images/logo.png";
 import { FaBars } from "react-icons/fa";
 
 const Header = () => {
+  const [showNavigation, setShowNavigation] = useState(false);
+  const toggleNavigation = () => {
+    setShowNavigation(!showNavigation);
+  };
   return (
     <div className="header">
       <div className="header__container">
         <img className="header__logo" src={logo} alt="step by step logo" />
-        <FaBars className="header__navbar" size={30} />
-        <nav className="header__navigation">
+        <FaBars
+          className="header__navbar"
+          size={30}
+          onClick={toggleNavigation}
+        />
+        <nav className={`header__navigation ${showNavigation ? "show" : ""}`}>
           <ul className="header__navigation-list">
             <li className="header__navigation-link">Home</li>
             <li className="header__navigation-link">About</li>
