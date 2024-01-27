cd080f5f9b4f65bdad02817c4bcd4aeb7e308d2a
Ralph <ralphdarync@gmail.com>
2024-01-26 19:53:41 -0500

    update hero section and react icons

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,30 +1,48 @@
 import "./Main.scss";
+import {
+  FaBirthdayCake,
+  FaPaintBrush,
+  FaBaby,
+  FaDumbbell,
+  FaRegCheckCircle,
+} from "react-icons/fa";
 
 const Main = () => {
   return (
     <div className="main">
       <div className="main__container">
         <div>
-          <h2 className="main__text">
+          <h2 className="main__subtitle">
             Elevate Your Celebration and Wellness with
           </h2>
-          <h1 className="main__text">Step By Step Stars!</h1>
+          <h1 className="main__title">Step By Step Stars!</h1>
         </div>
-        <p className="main__subtext">We offer the following services:</p>
         <div>
-          <ul>
-            <li>DANCE PARTIES (Ages 4+)</li>
-            <li>FACE PAINTING </li>
-            <li>MUSICAL BABIES (Ages 3 and under)</li>
-            <li>FITNESS CLASSES for MOMS</li>
+          
+        </div>
+        <div className="main__wrapper">
+          <p className="main__text">We offer the following services:</p>
+          <ul className="main__list">
+            <li className="main__link">
+              <FaBirthdayCake /> DANCE PARTIES <FaRegCheckCircle />
+            </li>
+            <li className="main__link">
+              <FaPaintBrush /> FACE PAINTING <FaRegCheckCircle />
+            </li>
+            <li className="main__link">
+              <FaBaby /> MUSICAL BABIES <FaRegCheckCircle />
+            </li>
+            <li className="main__link">
+              <FaDumbbell /> FITNESS CLASSES for MOMS <FaRegCheckCircle />
+            </li>
           </ul>
         </div>
         <div>
           <p className="main__subtext">
             Join us for unforgettable experiences!
           </p>
+          <button className="main__button">Book Now!</button>
         </div>
-        <button>Book Now!</button>
       </div>
     </div>
   );
