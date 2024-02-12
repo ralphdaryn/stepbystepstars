fc80a2aee37f0876b1a6a2df908654fedc7352bf
Ralph <ralphdarync@gmail.com>
2024-02-12 13:28:27 -0500

    update contact section, add form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,13 +1,69 @@
 import "./Contact.scss";
+import { useState } from "react";
 
 const Contact = () => {
+  // State to store form data
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  // Handle form input changes
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form data submitted:", formData);
+  };
+
   return (
     <div className="contact">
-      <h2>Contact</h2>
-      <p>Name</p>
-      <p>Email</p>
-      <p>Message</p>
+      <div className="contact__container">
+        <h2 className="contact__title">Contact</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="message">Message</label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleInputChange}
+            ></textarea>
+          </div>
+          <button className="contact__button" type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
+
 export default Contact;
