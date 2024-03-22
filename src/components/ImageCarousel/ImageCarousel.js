0672a2ca830e66a4413f1c9c8d60432e2b8e2e65
Ralph <ralphdarync@gmail.com>
2024-03-22 14:11:04 -0400

    update carousel timing

--- src/components/ImageCarousel/ImageCarousel.js
+++ src/components/ImageCarousel/ImageCarousel.js
@@ -15,7 +15,7 @@ const ImageCarousel = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 2000,
     centerMode: false,
     variableWidth: false,
     fade: true,
