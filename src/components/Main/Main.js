bfc78d6581b3b6f59ad5117f5511af9d22a593b0
Ralph <ralphdarync@gmail.com>
2024-05-07 15:48:53 -0400

    remove content

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,14 +1,6 @@
 import { useRef, useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import "./Main.scss";
-import {
-  FaBirthdayCake,
-  FaPaintBrush,
-  FaMusic,
-  FaDumbbell,
-  FaRegCheckCircle,
-} from "react-icons/fa";
-import { FaFacebook, FaInstagramSquare } from "react-icons/fa";
 import Carousel from "../ImageCarousel/ImageCarousel";
 import Services from "../Services/Services";
 import About from "../About/About";
@@ -69,63 +61,9 @@ const Main = () => {
   return (
     <div className="main">
       <div className="main__container">
-        <h1 className="main__title">
-          Elevate Your <span className="main__title-text">Celebration</span> &
-          <span className="main__title-text"> Wellness </span>
-          with
-          <span className="main__title-text"> Step By Step!</span>
-        </h1>
         <div>
           <Carousel />
         </div>
-        <div className="main__wrapper">
-          <h4 className="main__text">We offer the following services:</h4>
-          <div className="main__list">
-            <li className="main__list-item">
-              <FaBirthdayCake className="main__list-icon" /> DANCE PARTIES{" "}
-              <FaRegCheckCircle className="main__list-check" />
-            </li>
-            <li className="main__list-item">
-              <FaPaintBrush className="main__list-icon" /> FACE PAINTING{" "}
-              <FaRegCheckCircle className="main__list-check" />
-            </li>
-            <li className="main__list-item">
-              <FaMusic className="main__list-icon" /> MUSICAL BABIES{" "}
-              <FaRegCheckCircle className="main__list-check" />
-            </li>
-            <li className="main__list-item">
-              <FaDumbbell className="main__list-icon" /> FITNESS CLASSES{" "}
-              <FaRegCheckCircle className="main__list-check" />
-            </li>
-          </div>
-          <li className="main__list-item main__list-more">
-            ... and so much more!
-          </li>
-        </div>
-        <h3 className="main__subtext">
-          Join Us For{" "}
-          <span className="main__subtext-color">
-            Unforgettable Experiences!
-          </span>
-        </h3>
-        <div className="main__icons">
-          <ul className="main__icons-list">
-            <li className="main__icons-facebook">
-              <a href="https://www.facebook.com/stepbystepstars/">
-                <FaFacebook />
-              </a>
-            </li>
-            <a href="#contact" className="main__button">
-              Contact Us!
-            </a>
-            <li className="main__icons-instagram">
-              <a href="https://www.instagram.com/stepbystepstars/">
-                <FaInstagramSquare />
-              </a>
-            </li>
-          </ul>
-        </div>
-
         <div className="main__section">
           <ScrollFade key="services">
             <Services />
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -23,38 +23,10 @@ const Services = () => {
   return (
     <div id="services" className="services">
       <h2 className="services__text">
-        Start the Celebration! <br></br> Your{" "}
-        <span className="services__text-name">Step by Step</span> Journey Begins
-        Here!
+        Your <span className="services__text-name">Step by Step</span> Journey
+        Begins Here!
       </h2>
       <div className="services__container">
-        <div className="services__wrapper">
-          <div className="services__img-container">
-            <img
-              className="services__img-face"
-              src={facepaintImg}
-              alt="facepaint pic"
-            />
-          </div>
-          <h2 className="services__title">Facepaint</h2>
-          <p
-            className={`services__subtitle ${
-              showDetails.facepaint ? "show" : ""
-            }`}
-          >
-            Transform your celebration with our Face Painting Extravaganza! 🌈🎨
-            One hour of artistic magic that brings 8-12 kids to life with
-            creative designs. From whimsical butterflies to fierce superheroes,
-            our talented artists will make your party a canvas of joy. Book now
-            and let the face-painting fun begin!
-          </p>
-          <button
-            className="services__button"
-            onClick={() => toggleDetails("facepaint")}
-          >
-            {showDetails.facepaint ? "Hide Details" : "View Details"}
-          </button>
-        </div>
         <div className="services__wrapper">
           <div className="services__img-container">
             <img
@@ -85,6 +57,33 @@ const Services = () => {
         </div>
         <div className="services__wrapper">
           <div className="services__img-container">
+            <img
+              className="services__img-face"
+              src={facepaintImg}
+              alt="facepaint pic"
+            />
+          </div>
+          <h2 className="services__title">Face Painting</h2>
+          <p
+            className={`services__subtitle ${
+              showDetails.facepaint ? "show" : ""
+            }`}
+          >
+            Transform your celebration with our Face Painting Extravaganza! 🌈🎨
+            One hour of artistic magic that brings 8-12 kids to life with
+            creative designs. From whimsical butterflies to fierce superheroes,
+            our talented artists will make your party a canvas of joy. Book now
+            and let the face-painting fun begin!
+          </p>
+          <button
+            className="services__button"
+            onClick={() => toggleDetails("facepaint")}
+          >
+            {showDetails.facepaint ? "Hide Details" : "View Details"}
+          </button>
+        </div>
+        <div className="services__wrapper">
+          <div className="services__img-container">
             <img className="services__img" src={musicalImg} alt="musical pic" />
           </div>
           <h2 className="services__title">Musical Babies</h2>
@@ -110,7 +109,7 @@ const Services = () => {
           <div className="services__img-container">
             <img className="services__img" src={fitnessImg} alt="fitness pic" />
           </div>
-          <h2 className="services__title">Mommy & Me</h2>
+          <h2 className="services__title">Fitness Class</h2>
           <p
             className={`services__subtitle ${
               showDetails.mommyAndMe ? "show" : ""
