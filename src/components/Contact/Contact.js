3887b6482fe311bc2c40552d8fa6c63da5934580
Ralph <ralphdarync@gmail.com>
2024-05-09 12:36:43 -0400

    style contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,84 +1,3 @@
-// import "./Contact.scss";
-// import { useState } from "react";
-
-// const Contact = () => {
-//   const [formData, setFormData] = useState({
-//     name: "",
-//     email: "",
-//     message: "",
-//     phoneNumber: "", // New phone number field
-//   });
-
-//   const handleInputChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormData({
-//       ...formData,
-//       [name]: value,
-//     });
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     console.log("Form data submitted:", formData);
-//   };
-
-//   return (
-//     <div id="contact" className="contact">
-//       <div className="contact__container">
-//         <h2 className="contact__title">Contact</h2>
-//         <form onSubmit={handleSubmit}>
-//           <div>
-//             <label htmlFor="name">Name</label>
-//             <input
-//               type="text"
-//               id="name"
-//               name="name"
-//               value={formData.name}
-//               onChange={handleInputChange}
-//             />
-//           </div>
-
-//           <div>
-//             <label htmlFor="email">Email</label>
-//             <input
-//               type="email"
-//               id="email"
-//               name="email"
-//               value={formData.email}
-//               onChange={handleInputChange}
-//             />
-//           </div>
-
-//           <div>
-//             <label htmlFor="phoneNumber">Phone Number</label>
-//             <input
-//               type="text"
-//               id="phoneNumber"
-//               name="phoneNumber"
-//               value={formData.phoneNumber}
-//               onChange={handleInputChange}
-//             />
-//           </div>
-//           <div>
-//             <label htmlFor="message">Message</label>
-//             <textarea
-//               id="message"
-//               name="message"
-//               value={formData.message}
-//               onChange={handleInputChange}
-//             ></textarea>
-//           </div>
-//           <button className="contact__button" type="submit">
-//             Submit
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Contact;
-
 import "./Contact.scss";
 import { useState } from "react";
 
@@ -87,7 +6,7 @@ const Contact = () => {
     name: "",
     email: "",
     message: "",
-    phoneNumber: "", // New phone number field
+    phoneNumber: "", 
   });
 
   const handleInputChange = (e) => {
