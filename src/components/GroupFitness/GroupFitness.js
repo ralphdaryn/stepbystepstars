338b8bbf0634b1bb7339c6807c4ea0cd5e30ed4b
Ralph <ralphdarync@gmail.com>
2024-05-23 11:48:50 -0400

    style desktop view for mommyandme, private, group, contact

--- src/components/GroupFitness/GroupFitness.js
+++ src/components/GroupFitness/GroupFitness.js
@@ -5,18 +5,28 @@ const GroupFitness = () => {
   return (
     <div className="groupfitness">
       <h2 className="groupfitness__title">Group Fitness</h2>
-      <video className="groupfitness__video" controls autoPlay muted>
-        <source src={groupfitness} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      <p className="groupfitness__subtitle">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque ab amet
-        excepturi, sit laudantium consectetur placeat unde, veritatis quasi,
-        fugiat aliquam animi tenetur ut error? Ipsam perspiciatis necessitatibus
-        dolores veniam. Pariatur, quidem exercitationem voluptatibus excepturi
-        corrupti aliquam saepe consectetur, repellendus incidunt magni nam
-        magnam iusto in ipsa ea dolorum, eum nihil voluptatum recusandae.
-      </p>
+      <div className="groupfitness__container">
+        <video className="groupfitness__video" controls autoPlay muted>
+          <source src={groupfitness} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="groupfitness_wrapper">
+          <p className="groupfitness__subtitle">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            necessitatibus dolores veniam. Pariatur, quidem exercitatione.
+          </p>
+          <p className="groupfitness__subtitle">
+            voluptatibus excepturi corrupti aliquam saepe consectetur,
+            repellendus incidunt magni nam magnam iusto in ipsa ea dolorum, eum
+            nihil voluptatum recusandae.
+          </p>
+          <p className="groupfitness__subtitle">
+            voluptatibus excepturi corrupti aliquam saepe consectetur,
+            repellendus incidunt magni nam magnam iusto in ipsa ea dolorum, eum
+            nihil voluptatum recusandae.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/PrivateTraining/PrivateTraining.js
+++ src/components/PrivateTraining/PrivateTraining.js
@@ -5,19 +5,28 @@ const PrivateTraining = () => {
   return (
     <div className="private-training">
       <h2 className="private-training__title">Private Training</h2>
-      <video className="groupfitness__video" controls autoPlay muted>
-        <source src={privatetraining} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      <p className="private-training__subtitle">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi est unde
-        impedit cumque, repellendus veritatis ratione similique reprehenderit
-        molestiae nam expedita, modi eligendi optio saepe. Blanditiis repellat
-        harum tempore mollitia. Illum cumque assumenda eveniet. Sequi at
-        temporibus ipsa voluptatibus tempore fugiat totam iusto et quod eveniet,
-        excepturi rerum veniam? Cumque soluta voluptatum iste nisi. Officia amet
-        repellendus facilis debitis aspernatur!
-      </p>
+      <div className="private-training__container">
+        <video className="private-training__video" controls autoPlay muted>
+          <source src={privatetraining} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="private-training__wrapper">
+          <p className="private-training__subtitle">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. eveniet.
+            Sequi at temporibus ipsa voluptatibus tempore fugiat totam
+          </p>
+          <p className="private-training__subtitle">
+            Quasi est unde impedit cumque, repellendus veritatis ratione
+            similique reprehenderit molestiae nam expedita, modi eligendi optio
+            saepe.
+          </p>
+          <p className="private-training__subtitle">
+            iusto et quod eveniet, excepturi rerum veniam? Cumque soluta
+            voluptatum iste nisi. Officia amet repellendus facilis debitis
+            aspernatur!
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
