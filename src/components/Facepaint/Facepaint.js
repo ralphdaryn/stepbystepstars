c201befd0e2eee68ebd634ef861803a72dcb2c16
Ralph <ralphdarync@gmail.com>
2024-05-23 00:12:32 -0400

    style desktop view for danceparty, facepaint and musical

--- src/components/Facepaint/Facepaint.js
+++ src/components/Facepaint/Facepaint.js
@@ -7,32 +7,35 @@ const Facepaint = () => {
     <div className="facepaint">
       <h2 className="facepaint__title">Face Painting</h2>
       <div className="facepaint__container">
-        <img
-          src={facepaint1}
-          className="facepaint__image"
-          alt="Child with butterfly face paint"
-        />
-        <img
-          src={facepaint2}
-          className="facepaint__image"
-          alt="Child with superhero face paint"
-        />
-      </div>
-      <div className="facepaint__subtitle-container">
-        <p className="facepaint__subtitle">
-          Transform your celebration with our Face Painting Extravaganza! 🌈🎨
-        </p>
-        <p className="facepaint__subtitle">
-          One hour of artistic magic that brings 8-12 kids to life with creative
-          designs.
-        </p>
-        <p className="facepaint__subtitle">
-          From whimsical butterflies to fierce superheroes, our talented artists
-          will make your party a canvas of joy.
-        </p>
-        <p className="facepaint__subtitle">
-          Book now and let the face-painting fun begin!
-        </p>
+        <div className="facepaint__wrapper">
+          <img
+            src={facepaint1}
+            className="facepaint__image"
+            alt="Child with butterfly face paint"
+          />
+          <img
+            src={facepaint2}
+            className="facepaint__image"
+            alt="Child with superhero face paint"
+          />
+        </div>
+
+        <div className="facepaint__subtitle-container">
+          <p className="facepaint__subtitle">
+            Transform your celebration with our Face Painting Extravaganza! 🌈🎨
+          </p>
+          <p className="facepaint__subtitle">
+            One hour of artistic magic that brings 8-12 kids to life with
+            creative designs.
+          </p>
+          <p className="facepaint__subtitle">
+            From whimsical butterflies to fierce superheroes, our talented
+            artists will make your party a canvas of joy.
+          </p>
+          <p className="facepaint__subtitle">
+            Book now and let the face-painting fun begin!
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/components/MusicalBabies/Musicalbabies.js
+++ src/components/MusicalBabies/Musicalbabies.js
@@ -5,22 +5,26 @@ const Musicalbabies = () => {
   return (
     <div className="musicalbabies">
       <h2 className="musicalbabies__title">Musical Babies</h2>
-      <video className="musicalbabies__video" controls autoPlay muted>
-        <source src={musicalbabiesVideo} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      <div>
-        <p className="musicalbabies__subtitle">
-          Introducing our Musical Babies Program - a harmonious blend of joy and
-          learning for little ones aged 3 and under.
-        </p>
-        <p className="musicalbabies__subtitle">
-          Join us for an engaging half-hour session of interactive fun featuring
-          circle time, musical instruments, Montessori toys, parachute play!
-        </p>
-        <p className="musicalbabies__subtitle">
-          Watch your child blossom in a melody of laughter and discovery.
-        </p>
+      <div className="musicalbabies__container">
+        <video className="musicalbabies__video" controls autoPlay muted>
+          <source src={musicalbabiesVideo} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+
+        <div className="musicalbabies__wrapper">
+          <p className="musicalbabies__subtitle">
+            Introducing our Musical Babies Program - a harmonious blend of joy
+            and learning for little ones aged 3 and under.
+          </p>
+          <p className="musicalbabies__subtitle">
+            Join us for an engaging half-hour session of interactive fun
+            featuring circle time, musical instruments, Montessori toys,
+            parachute play!
+          </p>
+          <p className="musicalbabies__subtitle">
+            Watch your child blossom in a melody of laughter and discovery.
+          </p>
+        </div>
       </div>
     </div>
   );
