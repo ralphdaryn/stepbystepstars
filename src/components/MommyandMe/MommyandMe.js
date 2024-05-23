f83a82abe329527ef2218ae1718533d9d9864dd1
Ralph <ralphdarync@gmail.com>
2024-05-23 14:30:18 -0400

    add carousel inside mommyandme component

--- src/components/MommyandMe/MommyandMe.js
+++ src/components/MommyandMe/MommyandMe.js
@@ -1,3 +1,6 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "./MommyandMe.scss";
 import mommyandme1 from "../../assets/images/mommyandme1.jpeg";
 import mommyandme2 from "../../assets/images/mommyandme2.jpeg";
@@ -5,32 +8,37 @@ import mommyandme3 from "../../assets/images/mommyandme3.jpeg";
 import mommyandme4 from "../../assets/images/mommyandme4.jpeg";
 
 const MommyandMe = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    centerMode: false,
+    variableWidth: false,
+    fade: true,
+    pauseOnHover: false,
+  };
+
   return (
     <div className="mommyandme">
       <h2 className="mommyandme__title">Mommy and Me</h2>
-      <div className="mommyandme__container">
-        <img
-          src={mommyandme1}
-          className="mommyandme__image"
-          alt="Mom and baby exercising"
-        />
-        <img
-          src={mommyandme2}
-          className="mommyandme__image"
-          alt="Mom and baby stretching"
-        />
-        <img
-          src={mommyandme3}
-          className="mommyandme__image"
-          alt="Group of moms with babies"
-        />
-        <img
-          src={mommyandme4}
-          className="mommyandme__image"
-          alt="Mom and baby doing yoga"
-        />
-      </div>
-      <div>
+      <Slider {...settings} className="mommyandme__slider">
+        {[mommyandme1, mommyandme2, mommyandme3, mommyandme4].map(
+          (image, index) => (
+            <div key={index} className="mommyandme__container">
+              <img
+                className="mommyandme__image"
+                src={image}
+                alt={`Slide ${index + 1}`}
+              />
+            </div>
+          )
+        )}
+      </Slider>
+      <div className="mommyandme__wrapper">
         <p className="mommyandme__subtitle">
           Join our Mommy and Me Fitness Class, where all fitness levels are
           welcome!
