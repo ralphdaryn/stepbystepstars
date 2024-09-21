e4f62281856eee27b216ce11f703fd1a8c4759cd
ralphdaryn <ralphdarync@gmail.com>
2024-09-21 14:52:57 -0400

    update title

--- client/src/components/Danceparty/Danceparty.js
+++ client/src/components/Danceparty/Danceparty.js
@@ -8,7 +8,7 @@ const Danceparty = () => {
     <div className="danceparty">
       <div className="danceparty__header">
         <BackButton />
-        <h2 className="danceparty__title">Dance Party</h2>
+        <h2 className="danceparty__title">Birthday Parties</h2>
       </div>
       <div className="danceparty__container">
         <img className="danceparty__image" alt="dance party" src={danceparty} />
