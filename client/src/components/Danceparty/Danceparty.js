6e73fb041db615fe6560e43107eddd86d0ede83b
ralphdaryn <ralphdarync@gmail.com>
2024-09-26 21:17:34 -0400

    update image in service

--- client/src/components/Danceparty/Danceparty.js
+++ client/src/components/Danceparty/Danceparty.js
@@ -14,17 +14,15 @@ const Danceparty = () => {
         <img className="danceparty__image" alt="dance party" src={danceparty} />
         <div className="danceparty__subtitle-container">
           <p className="danceparty__subtitle">
-            Immerse in an hour of non-stop fun: dance parties that transform
-            into personalized music videos, engaging games with the kids,
-            choreographed dance moves for all to enjoy, and the glam of a red
-            carpet with a mini photoshoot.
+            Welcome to S TOWN, where kids can explore, play and learn through
+            interactive play! S TOWN - An indoor kids' village designed to
+            foster learning through pretend play. The space will include a
+            variety of interactive, miniature environments like a grocery store,
+            salon, veterinary office, and nostalgic 90s café, each encouraging
+            role-playing and imaginative exploration.
           </p>
           <p className="danceparty__subtitle">
-            Make your celebration unforgettable with our dynamic one-hour
-            entertainment service.
-          </p>
-          <p className="danceparty__subtitle">
-            Let's turn your party into a showstopper! 🕺💃🎥🎈
+            The design is crafted to allow your child to take the lead while you, as parents are meant to support and encourage their development. S Town promotes key developmental skill such as problem-solving, language development, social interaction, and cognitive flexibility. By engaging in these realistic, hands-on activities, children will not only have fun but also strengthen their understanding of real-world tasks and scenarios, helping them build social-emotional skills and boosting their creativity.
           </p>
           <div className="danceparty__button">
             <EventsButton />
