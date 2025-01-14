aed2c5d87b46a1a2ccf8ef7f181aafd17b25cb59
ralphdaryn <ralphdarync@gmail.com>
2025-01-13 21:12:40 -0500

    update drop in play button and link

--- client/src/components/EventsButton/EventsButton.js
+++ client/src/components/EventsButton/EventsButton.js
@@ -3,12 +3,12 @@ import "./EventsButton.scss";
 const EventsButton = () => {
   const handleButtonClick = () => {
     window.location.href =
-      "https://www.wellnessliving.com/rs/appointment-new/step_by_step_club?dtu_redirect=2024-12-31+14%3A54%3A22&id_class_tab=3&id_mode=1&k_class_tab=66298&s_id=OzPPUQ";
+      "https://www.wellnessliving.com/rs/appointment-new/step_by_step_club?dtu_redirect=2025-01-14+02%3A10%3A44&id_class_tab=3&id_mode=1&k_class_tab=66372&k_service=267754&k_service_category=61287&s_id=ZLOW6a";
   };
 
   return (
     <button className="events-button" onClick={handleButtonClick}>
-      Book Your Event
+      Reserve Your Spot
     </button>
   );
 };
