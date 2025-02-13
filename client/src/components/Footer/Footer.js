7909a8d10150ad9fa3f2902df438a7a1dbd63a66
ralphdaryn <ralphdarync@gmail.com>
2025-02-12 23:53:35 -0500

    update footer date

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,6 +2,8 @@ import "./Footer.scss";
 import { FaFacebook, FaInstagramSquare } from "react-icons/fa";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear(); 
+
   return (
     <div className="footer">
       <div className="footer__container">
@@ -11,15 +13,23 @@ const Footer = () => {
           ))}
         </div>
         <div className="footer__icons">
-          <a href="https://www.facebook.com/stepxstepclub/">
+          <a
+            href="https://www.facebook.com/stepxstepclub/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaFacebook className="footer__facebook" />
           </a>
-          <a href="https://www.instagram.com/stepxstepclub/">
+          <a
+            href="https://www.instagram.com/stepxstepclub/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaInstagramSquare className="footer__instagram" />
           </a>
         </div>
         <div>
-          <p className="footer__text">Step By Step © 2024</p>
+          <p className="footer__text">Step By Step © {currentYear}</p>
         </div>
       </div>
     </div>
