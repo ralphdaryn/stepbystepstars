6a6fb6550b6caa279e71a42901c6abcfb967ec73
ralphdaryn <ralphdarync@gmail.com>
2025-03-27 00:27:42 -0400

    update special events image and link

--- client/src/components/SpecialEvents/SpecialEvents.js
+++ client/src/components/SpecialEvents/SpecialEvents.js
@@ -6,15 +6,16 @@ import specialEvents2 from "../../assets/images/easter_egg.jpeg";
 import specialEvents3 from "../../assets/images/soca.jpeg";
 
 const SpecialEvents = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedMedia, setSelectedMedia] = useState(null);
 
-  const openImage = (imageSrc) => {
-    setSelectedImage(imageSrc);
+  const openMedia = (mediaSrc) => {
+    setSelectedMedia(mediaSrc);
   };
 
-  const closeImage = () => {
-    setSelectedImage(null);
+  const closeMedia = () => {
+    setSelectedMedia(null);
   };
+
   const linkToBuyTickets = () => {
     window.open(
       "https://www.wellnessliving.com/rs/event/step_by_step_club?k_class=783878&k_class_tab=66373",
@@ -36,35 +37,45 @@ const SpecialEvents = () => {
     );
   };
 
+  const linkToBuyTickets4 = () => {
+    window.open(
+      "https://www.wellnessliving.com/rs/event/step_by_step_club?k_class=787030&k_class_tab=68173",
+      "_blank"
+    );
+  };
+
   return (
     <div className="special-events">
       <div className="special-events__header">
         <BackButton />
         <h2 className="special-events__title">Special Events</h2>
       </div>
+
       <div className="special-events__intro">
         <p className="special-events__intro-text">
           Welcome to our Special Events! <br />
           Check out the exclusive events below to grab your tickets today!
         </p>
       </div>
+
       <div className="special-events__image-container">
         <img
           src={specialEvents3}
           alt="Special Event"
           className="special-events__image"
-          onClick={() => openImage(specialEvents3)}
+          onClick={() => openMedia(specialEvents3)}
         />
         <button className="special-events__button" onClick={linkToBuyTickets3}>
           Click Here To Buy Tickets!
         </button>
       </div>
+
       <div className="special-events__image-container">
         <img
           src={specialEvents}
           alt="Special Event"
           className="special-events__image"
-          onClick={() => openImage(specialEvents)}
+          onClick={() => openMedia(specialEvents)}
         />
         <button className="special-events__button" onClick={linkToBuyTickets}>
           Click Here To Buy Tickets!
@@ -76,24 +87,48 @@ const SpecialEvents = () => {
           src={specialEvents2}
           alt="Special Event"
           className="special-events__image"
-          onClick={() => openImage(specialEvents2)}
+          onClick={() => openMedia(specialEvents2)}
         />
         <button className="special-events__button" onClick={linkToBuyTickets2}>
           Click Here To Buy Tickets!
         </button>
       </div>
 
-      {/* Image Modal */}
-      {selectedImage && (
-        <div className="image-modal" onClick={closeImage}>
-          <span className="image-modal__close" onClick={closeImage}>
+      <div className="special-events__image-container">
+        <video
+          className="special-events__image"
+          autoPlay
+          muted
+          loop
+          playsInline
+          onClick={() => openMedia("/videos/boxing_poster.mp4")}
+        >
+          <source src="/videos/boxing_poster.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <button className="special-events__button" onClick={linkToBuyTickets4}>
+          Click Here To Buy Tickets!
+        </button>
+      </div>
+
+      {/* Media Modal */}
+      {selectedMedia && (
+        <div className="image-modal" onClick={closeMedia}>
+          <span className="image-modal__close" onClick={closeMedia}>
             &times;
           </span>
-          <img
-            className="image-modal__content"
-            src={selectedImage}
-            alt="Full View"
-          />
+          {selectedMedia.includes(".mp4") ? (
+            <video className="image-modal__content" controls autoPlay>
+              <source src={selectedMedia} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          ) : (
+            <img
+              className="image-modal__content"
+              src={selectedMedia}
+              alt="Full View"
+            />
+          )}
         </div>
       )}
     </div>
