2d4349591edf18824e0fc596ef72b3e726b5e7ff
ralphdaryn <ralphdarync@gmail.com>
2025-06-02 23:18:18 -0400

    upload image and update group fitness content

--- client/src/components/GroupFitness/GroupFitness.js
+++ client/src/components/GroupFitness/GroupFitness.js
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import "./GroupFitness.scss";
-import groupfitness from "../../assets/images/groupfitness.mp4";
 import BackButton from "../BackButton/BackButton";
+import groupFitnessImage from "../../assets/images/groupfitness1.jpeg";
+import groupFitnessImage2 from "../../assets/images/groupfitness-member.jpeg";
 
 const GroupFitness = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
   const handleButtonClick = () => {
-    window.open("https://www.wellnessliving.com/rs/catalog-list.html?a_shop_category%5B%5D=1102664&is_filter=1&k_business=651877");
+    window.open(
+      "https://www.wellnessliving.com/rs/catalog-list.html?a_shop_category%5B%5D=1102664&is_filter=1&k_business=651877"
+    );
+  };
+
+  const openImage = (imageSrc) => {
+    setSelectedImage(imageSrc);
+  };
+
+  const closeImage = () => {
+    setSelectedImage(null);
   };
 
   return (
@@ -14,11 +28,22 @@ const GroupFitness = () => {
         <h2 className="groupfitness__title">Group Fitness</h2>
       </div>
       <div className="groupfitness__container">
-        <video className="groupfitness__video" controls autoPlay muted>
-          <source src={groupfitness} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        <div className="groupfitness_wrapper">
+        <div className="groupfitness__wrapper">
+          <div className="groupfitness__image">
+            <img
+              src={groupFitnessImage}
+              alt="Group Fitness"
+              className="groupfitness__image-section"
+              onClick={() => openImage(groupFitnessImage)}
+            />
+            <img
+              src={groupFitnessImage2}
+              alt="Group Fitness Member"
+              className="groupfitness__image-section"
+              onClick={() => openImage(groupFitnessImage2)}
+            />
+          </div>
+
           <p className="groupfitness__subtitle">
             Boost your fitness with an energizing group workout that combines
             strength, cardio, and flexibility for a total body experience.
@@ -28,26 +53,31 @@ const GroupFitness = () => {
             intervals, functional movements, and core-focused exercises to
             maximize results. Each session is adaptable to individual fitness
             levels, making it perfect for beginners and experienced participants
-            alike. Bring your motivation and leave with a sense of
-            accomplishment, improved endurance, and increased strength!
+            alike.
           </p>
-          <p className="groupfitness__subtitle">
-            What to Expect:
-            <ul>
-              <li>• Dynamic warm-up to get the blood flowing.</li>
-              <li>• Varied circuits targeting different muscle groups.</li>
-              <li>• Core and balance work to enhance stability.</li>
-              <li>• Cool down and stretching to finish.</li>
-            </ul>
-          </p>
-          <div className="groupfitness__container">
+          <div className="groupfitness__button-container">
             <button
               className="groupfitness__button"
               onClick={handleButtonClick}
-            >Sign Up Today!</button>
+            >
+              Sign Up Today!
+            </button>
           </div>
         </div>
       </div>
+
+      {selectedImage && (
+        <div className="image-modal" onClick={closeImage}>
+          <span className="image-modal__close" onClick={closeImage}>
+            &times;
+          </span>
+          <img
+            className="image-modal__content"
+            src={selectedImage}
+            alt="Full View"
+          />
+        </div>
+      )}
     </div>
   );
 };
