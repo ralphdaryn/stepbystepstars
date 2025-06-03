6dd0dd7436c91d4fd6e6d6797f772f6b5791c9b2
ralphdaryn <ralphdarync@gmail.com>
2025-06-03 15:51:54 -0400

    update special events image and link

--- client/src/components/Alert/Alert.js
+++ client/src/components/Alert/Alert.js
@@ -3,7 +3,7 @@ import "./Alert.scss";
 const Alert = () => {
   return (
     <div className="alert">
-      🏋️ TAKE ADVANTAGE OF OUR FITNESS SPRING SALE!
+      🏋️ TAKE ADVANTAGE OF OUR FITNESS SPRING SALE! ⚡️
     </div>
   );
 };
--- client/src/components/SpecialEvents/SpecialEvents.js
+++ client/src/components/SpecialEvents/SpecialEvents.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import "./SpecialEvents.scss";
 import BackButton from "../BackButton/BackButton";
-import specialEvents5 from "../../assets/images/french_camp.jpeg";
-import specialEvents6 from "../../assets/images/summer_camp.jpeg";
+import specialEvents from "../../assets/images/french_camp.jpeg";
+import specialEvents1 from "../../assets/images/summer_camp.jpeg";
+import specialEvents2 from "../../assets/images/soca-june.jpeg";
 
 const SpecialEvents = () => {
   const [selectedMedia, setSelectedMedia] = useState(null);
@@ -10,16 +11,23 @@ const SpecialEvents = () => {
   const openMedia = (mediaSrc) => setSelectedMedia(mediaSrc);
   const closeMedia = () => setSelectedMedia(null);
 
-  const linkToBuyTickets5 = () => {
+  const linkToBuyTickets = () => {
     window.open(
       "https://www.wellnessliving.com/rs/book-process.html?id_book_process_back=8&id_class_tab=2&id_place=1&k_class_tab=66373&uid=56221530&dt_date=2025-07-07+13%3A00%3A00&k_class_period=15060258&k_business=651877&id_mode=1",
       "_blank"
     );
   };
 
-  const linkToBuyTickets6 = () => {
+  const linkToBuyTickets1 = () => {
     window.open(
       "  https://www.wellnessliving.com/rs/book-process.html?id_book_process_back=8&id_class_tab=2&id_place=1&k_class_tab=66373&uid=56221530&dt_date=2025-07-14+13%3A00%3A00&k_class_period=15060281&k_business=651877&id_mode=1",
+      "_blank"
+    );
+  };
+
+  const linkToBuyTickets2 = () => {
+    window.open(
+      "  https://www.wellnessliving.com/rs/catalog-view.html?k_business=651877&id_sale=3&k_id=813889",
       "_blank"
     );
   };
@@ -38,31 +46,42 @@ const SpecialEvents = () => {
         </p>
       </div>
 
-      <div className="special-events__card-wrapper"> 
+      <div className="special-events__card-wrapper">
         <div className="special-events__image-container">
           <img
-            src={specialEvents5}
+            src={specialEvents2}
             alt="Special Event"
             className="special-events__image"
-            onClick={() => openMedia(specialEvents5)}
+            onClick={() => openMedia(specialEvents2)}
           />
           <button
             className="special-events__button"
-            onClick={linkToBuyTickets5}
+            onClick={linkToBuyTickets2}
           >
             Click Here to Register!
           </button>
         </div>
         <div className="special-events__image-container">
           <img
-            src={specialEvents6}
+            src={specialEvents}
             alt="Special Event"
             className="special-events__image"
-            onClick={() => openMedia(specialEvents6)}
+            onClick={() => openMedia(specialEvents)}
+          />
+          <button className="special-events__button" onClick={linkToBuyTickets}>
+            Click Here to Register!
+          </button>
+        </div>
+        <div className="special-events__image-container">
+          <img
+            src={specialEvents1}
+            alt="Special Event"
+            className="special-events__image"
+            onClick={() => openMedia(specialEvents1)}
           />
           <button
             className="special-events__button"
-            onClick={linkToBuyTickets6}
+            onClick={linkToBuyTickets1}
           >
             Click Here to Register!
           </button>
