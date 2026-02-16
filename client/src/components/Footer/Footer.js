886b51fc070ac24d1f34e5baf8d714e6480e13b2
ralphdaryn <ralphdarync@gmail.com>
2026-02-16 11:59:13 -0500

    add dashboard link in footer section

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,8 +1,9 @@
 import "./Footer.scss";
+import { Link } from "react-router-dom";
 import { FaFacebook, FaInstagramSquare } from "react-icons/fa";
 
 const Footer = () => {
-  const currentYear = new Date().getFullYear(); 
+  const currentYear = new Date().getFullYear();
 
   return (
     <div className="footer">
@@ -12,6 +13,13 @@ const Footer = () => {
             <span key={i} />
           ))}
         </div>
+
+        {/* ✅ Subtle Client Login */}
+        <Link to="/dashboard" className="footer__client-link">
+          Client Login
+        </Link>
+
+        {/* ✅ Social Icons */}
         <div className="footer__icons">
           <a
             href="https://www.facebook.com/stepxstepclub/"
@@ -20,6 +28,7 @@ const Footer = () => {
           >
             <FaFacebook className="footer__facebook" />
           </a>
+
           <a
             href="https://www.instagram.com/stepxstepclub/"
             target="_blank"
@@ -28,9 +37,18 @@ const Footer = () => {
             <FaInstagramSquare className="footer__instagram" />
           </a>
         </div>
-        <div>
-          <p className="footer__text">Step By Step © {currentYear}</p>
-        </div>
+
+        {/* ✅ Address */}
+        <p className="footer__address">
+          1400 Bayly St. Unit 13B,
+          <br />
+          Pickering, ON
+          <br />
+          L1W 3R2
+        </p>
+
+        {/* ✅ Copyright */}
+        <p className="footer__text">Step By Step © {currentYear}</p>
       </div>
     </div>
   );
