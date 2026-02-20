eb7fb2b43d862e05a3ffd27fde65c13718b44f9e
ralphdaryn <ralphdarync@gmail.com>
2026-02-19 22:23:37 -0500

    add schooltrips and party rental event services page

---
+++ client/src/pages/PartyRentalsEventServices/PartyRentalsEventServices.js
@@ -0,0 +1,18 @@
+import "./PartyRentalsEventServices.scss";
+
+const PartyRentalsEventServices = () => {
+  return (
+    <div>
+      <h1>Party Rentals & Event Services</h1>
+      <p>
+        Bringing the fun to YOU. Hosting a birthday party, school fair,
+        community event, or backyard celebration? We now offer soft play
+        rentals, bouncy castles, and face painting services — delivered right to
+        your home, school, church, or event space. We can bring the S Town magic
+        to you!
+      </p>
+      <button>learn more email</button>
+    </div>
+  );
+};
+export default PartyRentalsEventServices;
---
+++ client/src/pages/SchoolTrips/SchoolTrips.js
@@ -0,0 +1,15 @@
+import "./SchoolTrips.scss";
+
+const SchoolTrips = () => {
+  return (
+    <div className="school-trips">
+      <h1>School Trips</h1>
+      <p>
+        We are open for school trips: Monday & Wednesday 9:30am - 1:00pm Friday
+        9:00am-3:00pm Add contact me or learn button and it will go to my email
+      </p>
+      <button>learn more email</button>
+    </div>
+  );
+};
+export default SchoolTrips;
