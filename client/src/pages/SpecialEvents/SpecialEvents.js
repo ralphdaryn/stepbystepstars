4cb5201520cb208bbaf02bca6099e1d749e63d2f
ralphdaryn <ralphdarync@gmail.com>
2026-03-03 08:45:02 -0500

    updagte special events image and link

--- client/src/pages/SpecialEvents/SpecialEvents.js
+++ client/src/pages/SpecialEvents/SpecialEvents.js
@@ -5,6 +5,7 @@ import specialEvents3 from "../../assets/images/smallgrouptrain.jpeg";
 import specialEvents4 from "../../assets/images/mommyme.jpeg";
 import specialEvents5 from "../../assets/images/stepclass.PNG";
 import specialEvents6 from "../../assets/images/stem.JPG";
+import specialEvents7 from "../../assets/images/littlechefs.JPG";
 
 const SpecialEvents = () => {
   const [selectedMedia, setSelectedMedia] = useState(null);
@@ -51,6 +52,13 @@ const SpecialEvents = () => {
     openRegistrationLink(
       "https://www.wellnessliving.com/rs/event/step_by_step_club?k_class=931466&k_class_tab=66373",
       "stem",
+    );
+  };
+
+  const linkToBuyTickets6 = () => {
+    openRegistrationLink(
+      "https://www.wellnessliving.com/rs/event/step_by_step_club?k_class=932980&k_class_tab=66373",
+      "littlechefs",
     );
   };
 
@@ -145,6 +153,22 @@ const SpecialEvents = () => {
           <button
             className="special-events__button"
             onClick={linkToBuyTickets5}
+          >
+            Click Here to Register!
+          </button>
+        </div>
+      </div>
+      <div className="special-events__card-wrapper">
+        <div className="special-events__image-container">
+          <img
+            src={specialEvents7}
+            alt="Special Event"
+            className="special-events__image"
+            onClick={() => openMedia(specialEvents7)}
+          />
+          <button
+            className="special-events__button"
+            onClick={linkToBuyTickets6}
           >
             Click Here to Register!
           </button>
