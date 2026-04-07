0a4a4b3e10cf46ef0d606f443d9819a85d813e2f
ralphdaryn <ralphdarync@gmail.com>
2026-04-06 21:36:14 -0400

    remove special events image and link

--- client/src/pages/SpecialEvents/SpecialEvents.js
+++ client/src/pages/SpecialEvents/SpecialEvents.js
@@ -3,7 +3,6 @@ import { useState } from "react";
 import specialEvents2 from "../../assets/images/soca.png";
 import specialEvents3 from "../../assets/images/smallgrouptrain.jpeg";
 import specialEvents4 from "../../assets/images/mommyme.jpeg";
-import specialEvents10 from "../../assets/images/easter.jpeg";
 
 const SpecialEvents = () => {
   const [selectedMedia, setSelectedMedia] = useState(null);
@@ -38,12 +37,7 @@ const SpecialEvents = () => {
       "soca",
     );
   };
-  const linkToBuyTickets9 = () => {
-    openRegistrationLink(
-      "https://www.wellnessliving.com/rs/event/step_by_step_club?k_class=936347&k_class_tab=66373",
-      "easter",
-    );
-  };
+ 
 
   return (
     <div className="special-events">
@@ -91,22 +85,7 @@ const SpecialEvents = () => {
           </button>
         </div>
       </div>
-      <div className="special-events__card-wrapper">
-        <div className="special-events__image-container">
-          <img
-            src={specialEvents10}
-            alt="Special Event"
-            className="special-events__image"
-            onClick={() => openMedia(specialEvents10)}
-          />
-          <button
-            className="special-events__button"
-            onClick={linkToBuyTickets9}
-          >
-            Click Here to Register!
-          </button>
-        </div>
-      </div>
+  
       <div className="special-events__card-wrapper">
         <div className="special-events__image-container">
           <img
